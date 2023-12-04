f347773776525d2ff1e9cf6ea73c8535297601da
Wipke9893 <Wipke9893@yahoo.com>
2023-12-04 18:00:53 -0500

    new

--- java/database/data.sql
+++ java/database/data.sql
@@ -4,7 +4,7 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 INSERT INTO public.breweries(user_id, name, address, address2, city, state_abbr, zip_code, phone, website, operation_hours, history, image)
-	VALUES (2, 'Griffin brew', '123 Main Street', '', 'Hamilton', 'OH', '45011', '5135555555', 'www.griffinbrew.com', 'M-F 2-11', 'no history', '');
+	VALUES (2, 'Griffin brew', '3301 Madison Rd', '', 'Cincinnati', 'Ohio', '45209', '5135555555', 'https://madtree.com/', 'M-F 2-11', 'no history', 'https://d2pxm94gkd1wuq.cloudfront.net/BreweryLogos/Standard/568634387.madtree.logo.green-squarepng.png');
 
 INSERT INTO public.breweries(user_id, name, address, address2, city, state_abbr, zip_code, phone, website, operation_hours, history, image)
     VALUES (2, 'Jack brew', '987 High Street', '', 'Cincinnati', 'OH', '45242', '5135555550', 'www.jackbrew.com', 'M-F 2-11', 'no history', '');
