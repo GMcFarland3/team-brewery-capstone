a914ff11ad6910360ac49834f917b08e76cb4e78
Logan Vesterfelt <loganvesterfelt@gmail.com>
2023-12-05 10:43:59 -0500

    m

--- vue/src/components/BreweryList.vue
+++ vue/src/components/BreweryList.vue
@@ -1,67 +1,67 @@
 <template>
-    <div>
-        <ul class="brewList">
-            <li>{{ breweries.name }}</li>
-            <li>{{ breweries.address }}</li>
-            <li>{{ breweries.address2 }}</li>
-            <li>{{ breweries.city }}</li>
-            <li>{{ breweries.state_abbr }}</li>
-            <li>{{ breweries.zip_code }}</li>
-            <li>{{ breweries.operation_hours }}</li>
-        </ul>
+    <div class="brewery-card">
+      <div class="image-box">
+        <img :src="breweries.image" @click="toggleDropdown" />
+      </div>
+      <select v-if="showDropdown" class="brewery-info">
+        <option value="name">{{ breweries.name }}</option>
+        <option value="address">{{ breweries.address }}</option>
+        <option value="city">{{ breweries.city }}</option>
+        <option value="state_abbr">{{ breweries.state_abbr }}</option>
+        <option value="zip">{{ breweries.zip }}</option>
+        <option value="phone">{{ breweries.phone }}</option>
+        <option value="website">{{ breweries.website }}</option>
+        <option value="operation_hours">{{ breweries.operation_hours }}</option>
+      </select>
     </div>
-    <div>
-        <h2>{{ breweries.image }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.address }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.city }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.state_abbr }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.zip }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.phone }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.website }}</h2>
-    </div>
-    <div>
-        <h2>{{ breweries.operation_hours }}</h2>
-    </div>
-</template>
-
-<script>
-
-
-export default {
+  </template>
+  
+  <script>
+  export default {
     props: ['breweries'],
-
-}
-</script>
-
-<style scoped>
-.brewList {
+    data() {
+      return {
+        showDropdown: false,
+      };
+    },
+    methods: {
+      toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .brewery-card {
     display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    gap: 15px;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+    border: 1px solid rgb(199, 170, 2);
+    padding: 10px;
+  }
+  
+  .image-box {
+    width: 150px; /* Adjust the width as needed */
+    height: 150px; /* Adjust the height as needed */
+    overflow: hidden;
+  }
+  
+  img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+  
+  .brewery-info {
+    display: none;
     text-align: left;
-    border-color: rgb(199, 170, 2);
-}
-
-
-li {
-    display: inline-flex;
-    text-align: left;
-    margin: 5px 5px 5px 5px;
-    padding: 5px 5px 5px 5px;
-    border-color: rgb(199, 170, 2);
-    color: rgb(199, 170, 2);
-}
-</style>
+  }
+  
+  .show-dropdown .brewery-info {
+    display: block;
+  }
+  </style>
+  
