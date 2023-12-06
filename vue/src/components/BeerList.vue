e9ff0d2ee35b661bf9a23bb3ccff120f615074dc
Logan Vesterfelt <loganvesterfelt@gmail.com>
2023-12-06 15:57:51 -0500

    m

--- vue/src/components/BeerList.vue
+++ vue/src/components/BeerList.vue
@@ -1,17 +1,5 @@
 <template>
     <div>
-<<<<<<< HEAD
-      <section class="beers-list">
-        <!-- Loop through the beers and display each one -->
-        <div v-for="beer in beers" :key="beer.beer_id" class="beer-item">
-          <img :src="beers.image" alt="Beer Image" />
-          <h3>{{ beers.name }}</h3>
-          <p>Type: {{ beers.type }}</p>
-          <p>Description: {{ beers.description }}</p>
-          <p>ABV: {{ beers.abv }}</p>
-        </div>
-      </section>
-=======
 
         <section class="beers-list" v-for="beer in beers" :key="beer.beer_id">
             <img :src="beer.image" alt="beer" />
@@ -23,28 +11,12 @@
             </ul>
         </section>
 
->>>>>>> d9666e1050898b9fec3d3ead3dee172f7411c79d
     </div>
   </template>
   
   <script>
   export default {
     props: ['beers'],
-<<<<<<< HEAD
-  };
-  </script>
-  
-  <style scoped>
-  /* Add your component-specific styles here */
-  .beer-item {
-    margin: 20px;
-    border: 1px solid #ddd;
-    padding: 10px;
-    display: inline-block;
-  }
-  </style>
-  
-=======
 };
 </script>
 
@@ -61,4 +33,3 @@ img {
 
 }
 </style>
->>>>>>> d9666e1050898b9fec3d3ead3dee172f7411c79d
