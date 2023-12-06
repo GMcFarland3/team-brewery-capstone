508a75951646f9763cea399544dea1992c89b4b3
Logan Vesterfelt <loganvesterfelt@gmail.com>
2023-12-06 14:13:26 -0500

    m

--- vue/src/services/BreweriesService.js
+++ vue/src/services/BreweriesService.js
@@ -2,13 +2,8 @@ import axios from 'axios';
 
 export default {
 
-<<<<<<< HEAD
-    getBreweries(){
-        return axios.getBreweries();
-=======
     getBreweries() {
         return axios.get('/breweries')
->>>>>>> df2e0524895bbafe843060c1443c482db54df4e1
     }
 
 }
